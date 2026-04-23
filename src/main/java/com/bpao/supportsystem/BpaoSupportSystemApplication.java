b3589636834196ba38575178c556d2284f94a5d0
BryanPool99 <bryanpool991@hotmail.com>
2026-04-23 11:36:44 -0500

    first commit

---
+++ src/main/java/com/bpao/supportsystem/BpaoSupportSystemApplication.java
@@ -0,0 +1,13 @@
+package com.bpao.supportsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BpaoSupportSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BpaoSupportSystemApplication.class, args);
+	}
+
+}
